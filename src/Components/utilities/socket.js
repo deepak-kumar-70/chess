e6595165b249f220d44c52f9e0483a234c44b167
deepak-kumar-70 <141209444+deepak-kumar-70@users.noreply.github.com>
2025-06-22 17:17:39 +0530

    h

--- src/Components/utilities/socket.js
+++ src/Components/utilities/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const socket = io(" https://chessbackend-y3cy.onrender.com", {
+const socket = io("https://chessbackend-y3cy.onrender.com", {
   transports: ['websocket'],
 });
 
